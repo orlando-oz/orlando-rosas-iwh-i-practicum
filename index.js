893730d1611848ac561cc66aa12614c6b6f8e4df
Ocozzio Developer <ocozziodeveloper@Ocozzio-Developer---MacBook-Pro.local>
2025-04-08 15:06:57 -0400

    removed "TODO" comments from index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
 const PRIVATE_APP_ACCESS = process.env.HUBSPOT_PRIVATE_APP_TOKEN;
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+// ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
 app.get('/', async (req, res)=>{
@@ -33,7 +33,7 @@ app.get('/', async (req, res)=>{
     }
 });
 
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
+// ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
 app.get('/update-cobj', (req, res)=>{
@@ -42,7 +42,7 @@ app.get('/update-cobj', (req, res)=>{
     });
 });
 
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
+// ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
 app.post('/update-cobj', async (req, res) => {
